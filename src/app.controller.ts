7235fabf6fc458e1c538a17acefe562f109f8e70
Manshimaro <power200012@naver.com>
2024-12-03 14:02:56 +0900

    Remove: "삭제 AppService"
    AppService 삭제

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,7 @@
 import { Controller, Get, Redirect, Render } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get('register')
   @Render('register')
   register() {
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
@@ -33,6 +32,5 @@ import { CategoriesModule } from './categories/categories.module';
     CategoriesModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
