5fb6b201ecbebb51dc20792a4af8b32f5602a47e
Manshimaro <power200012@naver.com>
2024-12-03 16:05:19 +0900

    Refactor: "아이템 변경 클라이언트 코드 리팩터링"
    아이템 변경 클라이언트 코드 리팩터링

--- public/item-change.js
+++ public/item-change.js
@@ -7,52 +7,55 @@ document.addEventListener('DOMContentLoaded', async () => {
     const name = getQueryParameter('name');
     const categorySelect = document.getElementById('category');
 
-    if (name) {
-        try {
-            const jwt = localStorage.getItem('jwt');
-            const response = await fetch(`/items/${name}`, {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${jwt}`,
-                }
-            });
-
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.message);
-            }
-
-            const response2 = await fetch('/categories', {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${jwt}`,
-                }
-            });
-
-            if (!response2.ok) {
-                const errorData = await response2.json();
-                throw new Error(errorData.message); 
-            }
-            
-            const itemData = await response.json();
-            const categoriesData = await response2.json();
-
-            categoriesData.forEach((category) => {
-                const option = document.createElement('option');
-                option.value = category.name;
-                option.textContent = category.name;
-                categorySelect.appendChild(option);
-            });
-
-            document.getElementById('name').value = itemData.name;
-            document.getElementById('number').value = itemData.number;
-            document.getElementById('expDate').value = itemData.expDate;
-            categorySelect.value = itemData.category;
-        } catch (error) {
-            alert(error);
-        }
-    } else {
+    if (!name) {
         alert('아이템이 지정되지 않았습니다.');
+        return;
+    }
+    
+    try {
+        const jwt = localStorage.getItem('jwt');
+
+        let response = await fetch(`/items/${name}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${jwt}`,
+            }
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.message);
+        }
+
+        const itemData = await response.json();
+
+        response = await fetch('/categories', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${jwt}`,
+            }
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.message); 
+        }
+        
+        const categoriesData = await response.json();
+
+        categoriesData.forEach((category) => {
+            const option = document.createElement('option');
+            option.value = category.name;
+            option.textContent = category.name;
+            categorySelect.appendChild(option);
+        });
+
+        document.getElementById('name').value = itemData.name;
+        document.getElementById('number').value = itemData.number;
+        document.getElementById('expDate').value = itemData.expDate;
+        categorySelect.value = itemData.category;
+    } catch (error) {
+        alert(error);
     }
 });
 
